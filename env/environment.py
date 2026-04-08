4b56bacfa1fd22deee4404f9ca93861a5ac0690d
Rithika K <rkrithika1506@gmail.com>
2026-04-08 21:21:02 +0530

    FINAL FIX: removed models.observation dependency

--- env/environment.py
+++ env/environment.py
@@ -1,29 +1,27 @@
 class EmailEnv:
     def __init__(self):
         self.step_count = 0
-        self.max_steps = 5
-        self.done = False
 
     def reset(self):
         self.step_count = 0
-        self.done = False
-        return {"email": "You won a free lottery! Click now!"}
+        return {
+            "email": "Congratulations! You won a prize. Click here now!"
+        }
 
     def step(self, action):
         self.step_count += 1
 
-        # Reward logic (dynamic, better scoring)
-        if action.value == "spam":
+        # simple reward logic
+        if action.get("value") == "spam":
             reward = 1.0
-        elif action.value == "not_spam":
-            reward = 0.5
         else:
             reward = 0.0
 
-        if self.step_count >= self.max_steps:
-            self.done = True
+        done = self.step_count >= 5
 
-        return {"email": "Next email content"}, reward, self.done, {}
-
-    def state(self):
-        return {"step": self.step_count}
+        return (
+            {"email": "Next email content"},
+            reward,
+            done,
+            {}
+        )
