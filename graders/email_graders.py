adc11de985f4da44f8e563fe65283ad9d21fcc3b
Rithika K <rkrithika1506@gmail.com>
2026-04-09 14:54:58 +0530

    Refactor grader functions to use ternary operator

--- graders/email_graders.py
+++ graders/email_graders.py
@@ -1,16 +1,10 @@
 def grader_easy(state, action):
-    if action.get("value") == "spam":
-        return 0.8
-    return 0.3
+    return 0.8 if action.get("value") == "spam" else 0.3
 
 
 def grader_medium(state, action):
-    if action.get("value") == "spam":
-        return 0.7
-    return 0.4
+    return 0.7 if action.get("value") == "spam" else 0.4
 
 
 def grader_hard(state, action):
-    if action.get("value") == "not_spam":
-        return 0.75
-    return 0.2
+    return 0.75 if action.get("value") == "not_spam" else 0.2
